8001bcf7c80e6c002b092ef841e3926ed3c06d94
Ermin Husic <Ermin.Husic@live.se>
2024-06-11 09:35:52 +0200

    update

--- Labb-6-Calculator/Calculator.cs
+++ Labb-6-Calculator/Calculator.cs
@@ -11,6 +11,7 @@ namespace Labb_6_Calculator
         public double Add(double a, double b) => a + b;
         public double Subtract(double a, double b) => a - b;
         public double Multiply(double a, double b) => a * b;
+        
         public double Divide(double a, double b)
         {
             if (b == 0)
